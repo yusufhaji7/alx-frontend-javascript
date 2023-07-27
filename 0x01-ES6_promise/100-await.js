aad8cbfe8c271ebceca70d791352b04d10d98631
yusufhaji7 <yusufhadji873@gmail.com>
2023-07-27 10:51:12 +0000

    advanced task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    try {
-	const photo = await uploadPhoto();
-	const user = await createUser();
-	return {
-	    photo,
-	    user,
-	};
-    } catch (error) {
-	return {
-	    photo: null,
-	    user: null,
-	};
-    }
+  try {
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
